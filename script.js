3d50e89d1371f99b0d9ae0d72e0c5730c0841aa5
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2025-01-10 22:14:48 +0700

    update lastest

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const bar = document.getElementById('bar')
+const close = document.getElementById('close')
+const nav = document.getElementById('navbar')
 
 if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
+  bar.addEventListener('click', () => {
+    nav.classList.add('active')
+  })
 }
 if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
+  close.addEventListener('click', () => {
+    nav.classList.remove('active')
+  })
 }
 
-var MainImg = document.getElementById("MainImg");
-var smalling = document.getElementsByClassName("small-img");
+var MainImg = document.getElementById('MainImg')
+var smallImg = document.getElementsByClassName('small-img')
 
-smalling[0].onclick = function () {
-    MainImg.src = smalling[0].src;
+smallImg[0].onclick = function () {
+  MainImg.src = smallImg[0].src
 }
-smalling[1].onclick = function () {
-    MainImg.src = smalling[1].src;
+smallImg[1].onclick = function () {
+  MainImg.src = smallImg[1].src
 }
-smalling[2].onclick = function () {
-    MainImg.src = smalling[2].src;
+smallImg[2].onclick = function () {
+  MainImg.src = smallImg[2].src
 }
-smalling[3].onclick = function () {
-    MainImg.src = smalling[3].src;
+smallImg[3].onclick = function () {
+  MainImg.src = smallImg[3].src
 }
